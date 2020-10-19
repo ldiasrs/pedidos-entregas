8d89a3c9b8c9edcb9e6e7067c9900e04825e6b40
Ldias <Ldias@gap.com>
2020-10-19 14:51:10 -0300

    Disable manualTests

--- src/test/java/com/aceleradora/pedidosentregas/client/GmailEmailSenderClientTest.java
+++ src/test/java/com/aceleradora/pedidosentregas/client/GmailEmailSenderClientTest.java
@@ -2,6 +2,7 @@ package com.aceleradora.pedidosentregas.client;
 
 import com.aceleradora.pedidosentregas.model.email.EmailRequest;
 import com.aceleradora.pedidosentregas.model.email.EmailResponse;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@Disabled
 class GmailEmailSenderClientTest {
 
     @Autowired
